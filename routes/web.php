771563ea87d92e980a948fe437045cd8780a54fe
ibrhmazmi <ibrhmazmi@gmail.com>
2023-09-12 23:51:35 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,11 +17,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 // login
-Route::get('/', [LoginController::class,'home']);
+Route::get('/', [LoginController::class, 'home'])->name('home');
+
+//auth
+Route::get('/Auth/{Social}',  [LoginController::class, 'redirectToSocial']);
+Route::get('/Auth/{Social}/Callback', [LoginController::class, 'handleSocialCallback']);
 
 //add middleware to avoid un-log personal
 // Todo
-Route::any('/Add', [TodoListController::class,'add']);
-Route::any('/Delete/{item_id}', [TodoListController::class,'delete']);
-Route::get('/List', [TodoListController::class,'list_all']);
-Route::any('/Mark-complete/{item_id}', [TodoListController::class,'mark_complete']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/Dashboard', [LoginController::class, 'dashboard']);
+    Route::any('/Logout', [LogoutController::class, 'logout'])->name('Logout');
+    Route::any('/Add', [TodoListController::class, 'add']);
+    Route::any('/Delete/{item_id}', [TodoListController::class, 'delete']);
+    Route::get('/List', [TodoListController::class, 'list_all']);
+    Route::any('/Mark-complete/{item_id}', [TodoListController::class, 'mark_complete']);
+});
