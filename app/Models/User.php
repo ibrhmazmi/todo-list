9793049d038d4dc21ed1a6be3f319e3d53413b7a
ibrhmazmi <ibrhmazmi@gmail.com>
2023-09-12 23:50:32 +0800

    update model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+
+class User extends Authenticatable
 {
     use HasFactory;
     protected $table = 'user';
     protected $primaryKey = 'user_id';
+    protected $fillable = [
+        'full_name',
+        'email',
+        'Facebook_id',
+        'Github_id',
+        'Google_id',
+        'avatar'
+    ];
 
 }
